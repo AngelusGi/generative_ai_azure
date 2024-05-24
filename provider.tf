7c6c1e596c6573da1ce1d12061c9994392710f23
angelo gino varrati <angelovarrati@hotmail.it>
2024-05-24 18:16:04 +0200

    update providers and backend

--- provider.tf
+++ provider.tf
@@ -1,3 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = var.state_rg_name
+    storage_account_name = var.state_storage_name
+    container_name       = var.state_container_name
+    key                  = "genai.tfstate"
+  }
+}
+
 provider "azurerm" {
-    features {}
+  features {
+    
+  }
 }
