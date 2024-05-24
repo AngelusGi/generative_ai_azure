ec4bb1a793bad1a4183ca2a0651a176d1b34f8ef
angelo gino varrati <angelovarrati@hotmail.it>
2024-05-24 18:16:51 +0200

    fix output apim-service

--- modules/api-management-service/output.tf
+++ modules/api-management-service/output.tf
@@ -4,16 +4,11 @@ output "name" {
 }
 
 output "url" {
-  value = azure_api_management.openai.management_api_url
+  value = azurerm_api_management.openai.management_api_url
   sensitive = false
 }
 
 output "dev_portal" {
-  value = azure_api_management.openai.portal_url
-  sensitive = false
-}
-
-output "openai_name" {
-  value = azurerm_cognitive_account.openai.name
+  value = azurerm_api_management.openai.portal_url
   sensitive = false
 }
